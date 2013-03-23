965cd51e9fb336093a88edb413059630a58e9c32
fission6 <fission6@fission6.(none)>
2013-03-23 14:06:07 -0400

    removed unwanted files

--- extension/eventPage.js
+++
@@ -1,6 +0,0 @@
-//In background.js:
-// React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    var tool_page = chrome.extension.getURL('tool.html');
-    chrome.tabs.create({url:tool_page});
-});
